6bc24eac3c557ccf6f6f8cafa027410332e1bd2b
Son Nguyen <son18122001@gmail.com>
2024-08-20 10:42:33 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
-
-const API_URL = 'https://jsonplaceholder.typicode.com';
-
-export const fetchUsers = async () => {
-    return await axios.get(`${API_URL}/users`);
-};
-
-export const fetchUserDetails = async (userId) => {
-    return await axios.get(`${API_URL}/users/${userId}`);
-};
-
-export const fetchUserPosts = async (userId) => {
-    return await axios.get(`${API_URL}/users/${userId}/posts`);
-};
-
-export const fetchUserAlbums = async (userId) => {
-    return await axios.get(`${API_URL}/users/${userId}/albums`);
-};
-
-export const fetchUserTodos = async (userId) => {
-    return await axios.get(`${API_URL}/users/${userId}/todos`);
-};
-
+// import axios from 'axios';
+//
+// const API_URL = 'https://jsonplaceholder.typicode.com';
+//
+// export const fetchUsers = async () => {
+//     return await axios.get(`${API_URL}/users`);
+// };
+//
+// export const fetchUserDetails = async (userId) => {
+//     return await axios.get(`${API_URL}/users/${userId}`);
+// };
+//
+// export const fetchUserPosts = async (userId) => {
+//     return await axios.get(`${API_URL}/users/${userId}/posts`);
+// };
+//
+// export const fetchUserAlbums = async (userId) => {
+//     return await axios.get(`${API_URL}/users/${userId}/albums`);
+// };
+//
+// export const fetchUserTodos = async (userId) => {
+//     return await axios.get(`${API_URL}/users/${userId}/todos`);
+// };
+//
