dabd8d2675289ccc5dd89f6a6efa0f9cdc456bfb
Son Nguyen <son18122001@gmail.com>
2024-09-18 10:24:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import {AppProvider} from '@shopify/polaris';
-import UserManagement from './pages/UserManagement';
-import UserDetail from './components/user/UserDetail';
-import PostDetail from './components/post/PostDetail';
-import AlbumDetail from './components/album/AlbumDetail';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { AppProvider } from "@shopify/polaris";
+import UserManagement from "./pages/UserManagement";
+import UserDetail from "./components/user/UserDetail";
+import PostDetail from "./components/post/PostDetail";
+import AlbumDetail from "./components/album/AlbumDetail";
 import UserPosts from "./components/post/UserPosts";
 import UserAlbums from "./components/album/UserAlbums";
 import UserTodos from "./components/todo/UserTodos";
 import AllPosts from "./components/navigation/AllPosts";
 import AllAlbums from "./components/navigation/AllAlbums";
 // import AllTodos from "./components/navigation/AllTodos";
-import NotFoundPage from './pages/NotFoundPage';
-import '@shopify/polaris/build/esm/styles.css';
+import NotFoundPage from "./pages/NotFoundPage";
+import "@shopify/polaris/build/esm/styles.css";
 import MainLayout from "./components/navigation/MainLayout";
 
-
 function App() {
-    return (
-        <AppProvider i18n={{}}>
-            <MainLayout>
-                <Routes>
-                    <Route path="/" exact element=<UserManagement/> />
-                    <Route path="/user/:id" element=<UserDetail/> />
-                    <Route path="/user/:userId/posts/:postId" element=<PostDetail/> />
-                    <Route path="/user/:userId/albums/:albumId" element=<AlbumDetail/> />
-                    <Route path="/user/:id/posts" element=<UserPosts /> />
-                    <Route path="/user/:id/albums" element=<UserAlbums /> />
-                    <Route path="/user/:id/todos" element=<UserTodos /> />
-                    <Route path="/posts" element=<AllPosts /> />
-                    <Route path="/albums" element=<AllAlbums /> />
-                    {/*<Route path="/todos" element=<AllTodos /> />*/}
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-            </MainLayout>
-        </AppProvider>
-    );
+  return (
+    <AppProvider i18n={{}}>
+      <MainLayout>
+        <Routes>
+          <Route path="/" exact element=<UserManagement /> />
+          <Route path="/user/:id" element=<UserDetail /> />
+          <Route path="/user/:userId/posts/:postId" element=<PostDetail /> />
+          <Route path="/user/:userId/albums/:albumId" element=<AlbumDetail /> />
+          <Route path="/user/:id/posts" element=<UserPosts /> />
+          <Route path="/user/:id/albums" element=<UserAlbums /> />
+          <Route path="/user/:id/todos" element=<UserTodos /> />
+          <Route path="/posts" element=<AllPosts /> />
+          <Route path="/albums" element=<AllAlbums /> />
+          {/*<Route path="/todos" element=<AllTodos /> />*/}
+          <Route path="*" element=<NotFoundPage /> />
+        </Routes>
+      </MainLayout>
+    </AppProvider>
+  );
 }
 
 export default App;
